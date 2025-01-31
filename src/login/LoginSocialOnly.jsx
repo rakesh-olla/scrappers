d39cde2799647d95ff6f13d34840aa563a9a269b
ap-developer-18 <puniaaman888@gmail.com>
2025-01-31 17:12:37 +0530

    change the widht of logic social only

--- src/login/LoginSocialOnly.jsx
+++ src/login/LoginSocialOnly.jsx
@@ -10,7 +10,7 @@ const LoginSocialOnly = () => {
         <div className='mt-14 md:mt-16 container xl:px-0 px-4'>
             <Heading text="Create an account" />
             <Paragraph text="We use LinkedIn or GitHub Login to verify you as a professional Developer / Agency" />
-            <form className='px-6 sm:px-8 md:px-10 lg:px-[52px] py-6 lg:py-7 rounded-[3px] bg-dark-400 border-[2px] border-dark-800 w-full max-w-[500px] mx-auto mt-5 md:mt-9'>
+            <form className='px-6 sm:px-8 md:px-10 lg:px-[52px] py-6 lg:pt-7 lg:pb-3 rounded-[3px] bg-dark-400 border-[2px] border-dark-800 w-full max-w-[500px] mx-auto mt-5 md:mt-9'>
                 <Paragraph className="mb-3 md:mb-4" text="Please fill use your LinkedIn or Github Account to register and verify!" />
                 <Link className='w-full mx-auto h-9 max-w-[271px] py-[6px] bg-white rounded-[3px] flex justify-center px-3'>
                     <img width={91} height={19} src={Linkedin} alt="Linkedin" />
